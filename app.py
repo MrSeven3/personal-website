1f5dfb517f73392adc4f4489e0599c400813ed04
thirtyseven <grayson@vantilborg.ca>
2026-05-17 20:54:06 -0400

    the stats are on the website!

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
+import mysql_utils
 
 app = Flask(__name__)
 
 @app.route("/")
 def da_main_page():
-    return render_template("index.html")
+    website_uptime = mysql_utils.get_website_uptime()
+    docker_services_online = mysql_utils.get_docker_services()
+
+    return render_template("index.html", docker_services=docker_services_online, uptime=website_uptime)
