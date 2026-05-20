635dd1e6e6e1b36b71210a31a190c44b4533bdb9
thirtyseven <grayson@vantilborg.ca>
2026-05-20 17:51:24 -0400

    more debug stuff :(

--- mysql_utils.py
+++ mysql_utils.py
@@ -65,6 +65,9 @@ def get_docker_data() -> list[int]:
         last_updated = existing_data[2]
         time_since_updated = datetime.datetime.now() - last_updated
 
+        print(f"db time:     {existing_data[2]}")
+        print(f"python now:  {datetime.datetime.now()}")
+
         if time_since_updated.total_seconds() <= int(existing_data[4])*60:
             return ast.literal_eval(existing_data[3])
         print("data is " + str(time_since_updated.total_seconds()) + " seconds old. refreshing stale data")
@@ -100,6 +103,9 @@ def get_website_uptime() -> float:
     if existing_data:
         last_updated = existing_data[2]
         time_since_updated = datetime.datetime.now() - last_updated
+
+        print(f"db time:     {existing_data[2]}")
+        print(f"python now:  {datetime.datetime.now()}")
 
         if time_since_updated.total_seconds() <= int(existing_data[4])*60:
             return float(existing_data[3])
