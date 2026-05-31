63e8f92404e3641aec4a218c283b709792acfad4
thirtyseven <grayson@vantilborg.ca>
2026-05-31 16:07:44 -0400

    can dynamically serve the list page

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from werkzeug.exceptions import HTTPException
 from flask import Flask, render_template
 from sentry_sdk.integrations.flask import FlaskIntegration
 from dotenv import load_dotenv
+import blog_utils
 import cache_utils
 import sentry_sdk
 import os
@@ -22,6 +23,15 @@ def da_main_page():
                            docker_containers=docker_data[1],
                            uptime=website_uptime
     )
+
+@app.route("/blogs")
+def blog_list():
+    blogs = blog_utils.get_blog_previews()
+
+    final_list = ""
+    for blog in blogs:
+        final_list += '<div class="blog-list-entry space-grotesk-normal"><a href="/blogs/'+blog[1]+'"><h2 class="space-grotesk-header text-highlight">'+blog[0]+'</h2></a><p>'+blog[2]+'</p></div>\n'
+    return render_template("/blog/blog_list.html", blog_list=final_list)
 
 @app.errorhandler(HTTPException)
 def fancy_error(error):
