3c64c2f5d8ae5c65766f3f38741d78f77b3d2227
thirtyseven <grayson@vantilborg.ca>
2026-06-21 11:02:57 -0400

    fix incorrect well known utils import

--- routes/well_known.py
+++ routes/well_known.py
@@ -7,8 +7,8 @@ dynamic_well_known_routes = Blueprint("dyn-well-known",__name__)
 def serve_dynamic_well_known(file):
     domain = urllib.parse.urlsplit(request.base_url).hostname
 
-    from utils import well_known_utils
-    contents = well_known_utils.get_well_known_entry(file, domain)
+    import utils.well_known
+    contents = utils.well_known.get_well_known_entry(file, domain)
     if contents is None: return {"error":"well_known_not_found"}, 404
 
     return Response(contents, mimetype="text/plain")
