181fc7e66498bd224b4235c83cb65a7ac235a9ad
thirtyseven <grayson@vantilborg.ca>
2026-07-01 10:40:54 -0400

    move website uptime gathering to separate function

--- utils/cache.py
+++ utils/cache.py
@@ -82,32 +82,26 @@ def get_docker_data() -> list[int]:
     else:
         return [-1, -1]
 
-def get_website_uptime() -> float:
-    existing_data = get_cache_data("website_uptime")
-    if existing_data:
-        last_updated = existing_data[2]
-        time_since_updated = datetime.datetime.now() - dateparser.parse(last_updated)
-
-        if time_since_updated.total_seconds() <= int(existing_data[4])*60: #check if i can use the existing data, and return it if i can
-            return float(existing_data[3])
-        print("data is " + str(time_since_updated.total_seconds()) + " seconds old. refreshing stale data")
-
-    grafana_auth_headers = {"Authorization": "Bearer "+os.environ.get("GRAFANA_API_KEY")}
+def fetch_website_uptime():
+    grafana_auth_headers = {"Authorization": "Bearer " + os.environ.get("GRAFANA_API_KEY")}
 
     try: #pull the uptime data from prometheus using grafana. prometheus gets it from uptime kuma, tho the calculations are different, so this is slightly wrong. TODO: either fix prometheus or find a way to poll uptime kuma directly
         uptime_data = requests.get("https://grafana.thirtyseventh.xyz/api/datasources/uid/efive1u0b5wqob/resources/api/v1/query?query=avg_over_time(monitor_status%7Bmonitor_name%3D%22Personal%20Website%22%7D%5B30d%5D)", headers=grafana_auth_headers, timeout=(1, 2))
         if uptime_data.status_code != 200:
-            print("uptime data get failed with code " +str(uptime_data.status_code)+"\n"+str(uptime_data.text))
-            float(existing_data[3]) if existing_data else -1 #if its available, return old data on new data fail
-    except requests.exceptions.Timeout:
-        sentry_sdk.capture_message(
-            "uptime data gathering timed out",
-            level="warning",
-        )
-        return float(existing_data[3]) if existing_data else -1#if its available, return old data on new data fail
+            sentry_sdk.capture_message("uptime data gathering did not return sucessfully, returned code "+str(uptime_data.status_code), level="warning")
+            return
+    except requests.exceptions.Timeout or requests.exceptions.ConnectionError:
+        sentry_sdk.capture_message("uptime data gathering failed to connect to grafana",level="warning")
+        return
 
     uptime = float(uptime_data.json()['data']['result'][0]['value'][1]) * 100
     uptime = round(uptime, 2)
 
     store_cache_data("website_uptime", str(uptime))
-    return uptime
+
+def get_website_uptime() -> float:
+    existing_data = get_cache_data("website_uptime")
+    if existing_data:
+        return float(existing_data[3])
+    else:
+        return float(-1)
